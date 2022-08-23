b98cb9a0d7c67056f38faf41528bb2c62c2cae49
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-23 20:31:30 +0300

    update startup.cs to auto migrate

--- colours/Startup.cs
+++ colours/Startup.cs
@@ -1,4 +1,5 @@
 using colours.Data;
+using colours.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace colours
@@ -27,14 +29,21 @@ namespace colours
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var server = Configuration["DBServer"] ?? "localhost";
+            var server = Configuration["DBServer"] ?? "ms-sql-server";
             var port = Configuration["DBPort"] ?? "1433";
             var user = Configuration["DBUser"] ?? "SA";
             var password = Configuration["DBPassword"] ?? "P@ssw0rd";
-            var database = Configuration["DayaBase"] ?? "colours-db";
+            var database = Configuration["DataBase"] ?? "colours-db";
 
-            services.AddDbContext<ApplicationDbContext>(options => 
-            options.UseSqlServer($"Server={server},{port}; Initial Catalog={database}; User ID={user}; Password={password}"));
+            services.AddDbContext<ApplicationDbContext>(
+                options =>
+                options.UseSqlServer($"Server={server},{port}; Initial Catalog={database}; ID={user}; Password={password};Trusted_Connection=True; MultipleActiveResultSets=True;"
+                    //b =>
+                    //b.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name)
+                )
+            );
+
+            
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -44,8 +53,16 @@ namespace colours
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,
+            IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceProvider.GetService<ApplicationDbContext>().Database.EnsureDeleted();
+                serviceProvider.GetService<ApplicationDbContext>().Database.Migrate();
+            }
+                
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -61,6 +78,14 @@ namespace colours
             {
                 endpoints.MapControllers();
             });
+
+            //using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            //{
+            //    var context1 = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+            //    context1.Database.Migrate();
+            //    context1.SaveChanges();
+            //}
+            //serviceProvider.GetService<ApplicationDbContext>().Database.Migrate();
         }
     }
 }
